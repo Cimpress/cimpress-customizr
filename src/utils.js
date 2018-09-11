5a9ddc9c425e5941a1219f4b7d54815b98bf4c06
Ivan Stanishev <istanishev@cimpress.com>
2018-09-11 10:08:05 +0200

    - Added `setPreferredMcpSettings` helper to valide and update all users settings in a single call

---
+++ src/utils.js
@@ -0,0 +1,50 @@
+import countryLanguage from 'country-language';
+import IANATimezoneData from "iana-tz-data";
+
+const validTimezones = [];
+const regions = Object.keys(IANATimezoneData.zoneData);
+regions.forEach((region) => {
+    Object.keys(IANATimezoneData.zoneData[region])
+        .forEach((city) => {
+            validTimezones.push(`${region}/${city}`);
+        });
+});
+
+function getValidLanguageOrThrow(languageCode) {
+    let language = countryLanguage.getLanguages()
+        .find((a) => (a.iso639_1 && a.iso639_1 === languageCode)
+            || (a.iso639_2 && a.iso639_2 === languageCode)
+            || (a.iso639_3 && a.iso639_3 === languageCode));
+
+    if (!language) {
+        throw new Error('Provided language code is not valid. Please pass a valid ISO-639 code');
+    }
+
+    return language;
+}
+
+function getValidTimezoneOrThrow(timezone) {
+    let tz = validTimezones.find((t) => t === timezone);
+    if (!tz) {
+        throw new Error('Provided timezone is not valid. Please pass valid IANA timezone identifier, eg. Europe/Amsterdam');
+    }
+    return tz;
+}
+
+function getValidLanguageTagOrThrow(languageTag) {
+    // TODO
+    return languageTag;
+}
+
+function updatePreferredLanguage(preferredLanguage, currentLanguages) {
+    return [preferredLanguage.iso639_1].concat(currentLanguages.filter((a) => a !== preferredLanguage.iso639_1));
+}
+
+
+export {
+    getValidLanguageOrThrow,
+    getValidLanguageTagOrThrow,
+    getValidTimezoneOrThrow,
+
+    updatePreferredLanguage,
+};
