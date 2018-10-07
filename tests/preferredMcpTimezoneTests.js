0da9cd29e63903ce69006c26c9eb940fe55a8f59
Ivan Stanishev <istanishev@cimpress.com>
2018-10-07 21:11:15 +0200

    Fix timezone validation to allow timezones with non-Region/City format.

--- tests/preferredMcpTimezoneTests.js
+++ tests/preferredMcpTimezoneTests.js
@@ -26,6 +26,12 @@ describe('Timezone', function() {
                     // empty
                 });
         });
+        it('should accept a valid timezone', function() {
+            return setPreferredMcpTimezone(token, 'W-SU')
+                .then(() => {
+                    // empty
+                });
+        });
         it('should fail with incorrect timezone', function() {
             return setPreferredMcpTimezone(token, 'wrong/timezone')
                 .then((timezone) => {
