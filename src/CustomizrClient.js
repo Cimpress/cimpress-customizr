6281ee76ece59249832bdd27ed22440c5960820a
Ivan Stanishev <istanishev@cimpress.com>
2019-01-17 22:23:15 +0100

    Add missing comma

--- src/CustomizrClient.js
+++ src/CustomizrClient.js
@@ -40,7 +40,7 @@ class CustomizrClient {
                 retryDelay: (retryCount) => {
                     return this.retryDelayInMs;
                 },
-                shouldResetTimeout: true
+                shouldResetTimeout: true,
             });
         }
 
